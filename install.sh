4c46b42e995ec886169ff05a2cd3afea4fa683f0
Umamaheswararao Kankanampati <umamaheswararao.kankanampati@tectoro.com>
2026-02-11 12:55:22 +0530

    provide permission for bash files

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 echo "Installing mongodb container"
+chmod +x ./mongo/podman_install.sh
 ./mongo/podman_install.sh
 
 echo "Installing nginx container"
+chmod +x ./nginx/install_nginx.sh
 ./nginx/install_nginx.sh    
 
 echo "Installing haproxy container"
+chmod +x ./haproxy/install_haproxy.sh
 ./haproxy/install_haproxy.sh
 
 echo "Installing cache and mq services"
+chmod +x ./cache_mq_server/podman_install.sh
 ./cache_mq_server/podman_install.sh
